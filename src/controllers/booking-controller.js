d04e6cfea4c6ced779e0463688e728a8a5271803
Samir Kumar Roy <samirroy594@gmail.com>
2023-03-13 16:16:02 +0600

    message queue setup with Rabbit amqplib is done to send reminder

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -3,26 +3,50 @@ const { StatusCodes } = require('http-status-codes');
 
 const bookingService = new BookingService();
 
+const {createChannel, publishMessage}=require('../utils/message-queue');
+const {REMINDER_BINDING_KEY}=require('../config/serverConfig');
+class BookingController{
+                            constructor(){
+                            }
+                            async sendMessageToQueue(req,res){
+                                const channel = await createChannel();
+                                const payload = {
+                                    data: {
+                                        subject: 'this is a notification to be comsumed',
+                                        content: 'Some queue will subscribe to this event',
+                                        recepientEmail: 'praptesarker@gmail.com',
+                                        notificationTime: '2023-03-09 13:20:30'
+                                    },
+                                    service: 'CREATE_TICKET'
+                                };
+                                publishMessage(channel,REMINDER_BINDING_KEY,JSON.stringify(payload));
+                                return res.status(201).json({
+                                    Message: "Successfully published the event"
 
-const create = async (req, res) => {
-    try {
-        const response = await bookingService.createBooking(req.body);
-        return res.status(StatusCodes.OK).json({
-            message: "Flight booking is completed successfully",
-            success: true,
-            data: response,
-            error: {}
-        });
-    } catch (err) {
-        return res.status(err.statusCode).json({
-            message: err.message,
-            success: false,
-            data: {},
-            error: err.explanation
-        });
-    }
-}
+                                });
 
-module.exports = {
-    create
-}
+                            }
+
+
+                            async create(req,res){
+                                                    try {
+                                                            const response = await bookingService.createBooking(req.body);
+                                                            return res.status(StatusCodes.OK).json({
+                                                                message: "Flight booking is completed successfully",
+                                                                success: true,
+                                                                data: response,
+                                                                error: {}
+                                                                });
+                                                        } catch (err) {
+                                                                        return res.status(err.statusCode).json({
+                                                                            message: err.message,
+                                                                            success: false,
+                                                                            data: {},
+                                                                            error: err.explanation
+                                                                        });
+                                                                        }
+                                                }
+
+                        }
+
+module.exports = BookingController;
